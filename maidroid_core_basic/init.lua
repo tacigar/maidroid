6f7b74445c6ae19ac44493a89dd14482825d346c
tacigar <ig.lhound@gmail.com>
2016-09-13 17:49:35 +0900

    [UPDATE] First commit

---
+++ maidroid_core_basic/init.lua
@@ -0,0 +1,11 @@
+------------------------------------------------------------
+-- Copyright (c) 2016 tacigar. All rights reserved.
+-- https://github.com/tacigar/maidroid
+------------------------------------------------------------
+
+maidroid_core_basic = {}
+
+maidroid_core_basic.modname = "maidroid_core_basic"
+maidroid_core_basic.modpath = minetest.get_modpath(maidroid_core_basic.modname)
+
+dofile(maidroid.modpath .. "/register.lua")
