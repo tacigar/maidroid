998c9bd46abf9ad6054541550c87cdf535cacfcd
tacigar <ig.lhound@gmail.com>
2016-09-14 09:35:28 +0900

    [ADD] Add core_writer

---
+++ maidroid_tool/core_writer.lua
@@ -0,0 +1,11 @@
+------------------------------------------------------------
+-- Copyright (c) 2016 tacigar. All rights reserved.
+-- https://github.com/tacigar/maidroid
+------------------------------------------------------------
+
+-- register a definition of a core writer.
+minetest.register_node("maidroid_tool:core_writer", {
+	drawtype        = "nodebox",
+	tiles           = {},
+
+})
