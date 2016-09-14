99fd18ec0db00b396727c5b8309e7f7a90c11a9e
tacigar <ig.lhound@gmail.com>
2016-09-14 18:52:56 +0900

    [UPDATE] Commit before implementing core writer callbacks

--- maidroid_tool/core_writer.lua
+++ maidroid_tool/core_writer.lua
@@ -15,28 +15,70 @@
 			{  0.375,  0.1875,  0.0625,  0.4375,   0.375,   0.125},
 			{ -0.375,    -0.5,  -0.375,   0.375,   -0.25,   0.375},
 		},
-	}
+	};
 
-	local tiles = {
-		"maidroid_tool_core_writer_top.png",
-		"maidroid_tool_core_writer_bottom.png",
-		"maidroid_tool_core_writer_right.png",
-		"maidroid_tool_core_writer_right.png^[transformFX",
-		"maidroid_tool_core_writer_front.png^[transformFX",
-		"maidroid_tool_core_writer_front.png",
-	}
+	function allow_metadata_inventory_put(pos, listname, index, stack, player)
 
-	minetest.register_node("maidroid_tool:core_writer", {
-		description        = "maidroid tool : core writer",
-		drawtype           = "nodebox",
-		paramtype          = "light",
-		paramtype2         = "facedir",
-		groups             = {cracky = 2},
-		is_ground_content  = false,
-		sounds             = default.node_sound_stone_defaults(),
-		node_box           = node_box,
-		tiles              = tiles,
-	})
+	end
+
+	function allow_metadata_inventory_move(pos, from_list, from_index, to_list, to_index, count, player)
+
+	end
+
+	function allow_metadata_inventory_take(pos, listname, index, stack, player)
+
+	end
+
+	(function() -- register a definition of an inactive core writer.
+		local tiles = {
+			"maidroid_tool_core_writer_top.png",
+			"maidroid_tool_core_writer_bottom.png",
+			"maidroid_tool_core_writer_right.png",
+			"maidroid_tool_core_writer_right.png^[transformFX",
+			"maidroid_tool_core_writer_front.png^[transformFX",
+			"maidroid_tool_core_writer_front.png",
+		}
+
+		local formspec_string =	"size[8,9]"
+			.. "list[current_player;main;0,5;8,1;]"
+			.. "list[current_player;main;0,6.2;8,3;8]"
+
+		function on_construct(pos)
+			local meta = minetest.get_meta(pos)
+			meta:set_string("formspec", formspec_string)
+
+			local inventory = meta:get_inventory()
+			inventory:set_size("core", 1)
+			inventory:set_size("fuel", 1)
+			inventory:set_size("dye", 1)
+		end
+
+		function on_metadata_inventory_move = function(pos)
+
+		end
+
+		minetest.register_node("maidroid_tool:core_writer", {
+			description                    = "maidroid tool : core writer",
+			drawtype                       = "nodebox",
+			paramtype                      = "light",
+			paramtype2                     = "facedir",
+			groups                         = {cracky = 2},
+			is_ground_content              = false,
+			sounds                         = default.node_sound_stone_defaults(),
+			node_box                       = node_box,
+			tiles                          = tiles,
+			can_dig                        = can_dig,
+			on_construct                   = on_construct,
+			on_metadata_inventory_move     = on_metadata_inventory_move,
+			allow_metadata_inventory_put   = allow_metadata_inventory_put,
+			allow_metadata_inventory_move  = allow_metadata_inventory_move,
+			allow_metadata_inventory_take  = allow_metadata_inventory_take,
+		})
+	end) ();
+
+	(function () -- register a definition of an active core writer.
+
+	end) ();
 end) ();
 
 -- register a definition of a core entity.
@@ -73,7 +115,7 @@ end) ();
 		physical       = false,
 		visual         = "wielditem",
 		visual_size    = {x = 0.5, y = 0.5},
-		collisionbox  = {0, 0, 0, 0, 0, 0},
+		collisionbox   = {0, 0, 0, 0, 0, 0},
 
 		on_activate = function(self, staticdata)
 			self.object:set_properties{textures = {"maidroid_tool:core_node"}}
@@ -84,9 +126,4 @@ end) ();
 			self.object:setyaw(yaw + 0.1)
 		end,
 	})
-
-	minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-		minetest.add_entity(pointed_thing.above, "maidroid_tool:core_entity")
-	end)
-
-end) ()
+end) ();
