a6d0f99ffaf26a0c01b48d1b5586a1adbaa482e5
tacigar <ig.lhound@gmail.com>
2016-09-15 10:47:09 +0900

    [ADD] Add recipe

--- maidroid_tool/init.lua
+++ maidroid_tool/init.lua
@@ -9,3 +9,4 @@ maidroid_tool.modname = "maidroid_tool"
 maidroid_tool.modpath = minetest.get_modpath(maidroid_tool.modname)
 
 dofile(maidroid_tool.modpath .. "/core_writer.lua")
+dofile(maidroid_tool.modpath .. "/crafting.lua")
