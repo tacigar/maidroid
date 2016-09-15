2505e6098493480c9fa3bdd25526c4a22ea6f2f9
tacigar <ig.lhound@gmail.com>
2016-09-15 10:57:48 +0900

    [FIX] Fix spawner

--- maidroid/register.lua
+++ maidroid/register.lua
@@ -4,22 +4,25 @@
 ------------------------------------------------------------
 
 maidroid.register_maidroid("maidroid:maidroid_mk1", {
-	hp_max    = 10,
-	weight    = 20,
-	mesh      = "maidroid.b3d",
-	textures  = {"maidroid_mk1.png"},
+	hp_max           = 10,
+	weight           = 20,
+	mesh             = "maidroid.b3d",
+	textures         = {"maidroid_mk1.png"},
+	inventory_image  = "maidroid_mk1_spawner.png",
 })
 
 maidroid.register_maidroid("maidroid:maidroid_mk2", {
-	hp_max    = 10,
-	weight    = 20,
-	mesh      = "maidroid.b3d",
-	textures  = {"maidroid_mk2.png"},
+	hp_max           = 10,
+	weight           = 20,
+	mesh             = "maidroid.b3d",
+	textures         = {"maidroid_mk2.png"},
+	inventory_image  = "maidroid_mk2_spawner.png",
 })
 
 maidroid.register_maidroid("maidroid:maidroid_mk3", {
-	hp_max    = 10,
-	weight    = 20,
-	mesh      = "maidroid.b3d",
-	textures  = {"maidroid_mk3.png"},
+	hp_max           = 10,
+	weight           = 20,
+	mesh             = "maidroid.b3d",
+	textures         = {"maidroid_mk3.png"},
+	inventory_image  = "maidroid_mk3_spawner.png",
 })
