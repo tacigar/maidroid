e694f394d1b97eee7ef8d6d5157a5b4977f848a7
tacigar <ig.lhound@gmail.com>
2016-12-08 21:47:15 +0900

    create farming core schema

--- maidroid_core/cores/farming.lua
+++ maidroid_core/cores/farming.lua
@@ -2,3 +2,33 @@
 -- Copyright (c) 2016 tacigar. All rights reserved.
 -- https://github.com/tacigar/maidroid
 ------------------------------------------------------------
+
+local state = {
+
+}
+
+local function on_start(self)
+
+end
+
+local function on_stop(self)
+
+end
+
+local on_resume = on_start
+
+local on_pause = on_stop
+
+local function on_step(self, dtime)
+
+end
+
+maidroid.register_core("maidroid_core:farming", {
+	description      = "maidroid core : farming",
+	inventory_image  = "maidroid_core_farming.png",
+	on_start         = on_start,
+	on_stop          = on_stop,
+	on_resume        = on_resume,
+	on_pause         = on_pause,
+	on_step          = on_step,
+})
