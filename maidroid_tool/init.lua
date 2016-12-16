e4118f08248bd037e255454586dd19f300e78e70
tacigar <ig.lhound@gmail.com>
2016-12-16 16:04:52 +0900

    Update maidroid_tool/init.lua

--- maidroid_tool/init.lua
+++ maidroid_tool/init.lua
@@ -8,5 +8,6 @@ maidroid_tool = {}
 maidroid_tool.modname = "maidroid_tool"
 maidroid_tool.modpath = minetest.get_modpath(maidroid_tool.modname)
 
+dofile(maidroid_tool.modpath .. "/api.lua")
 dofile(maidroid_tool.modpath .. "/core_writer.lua")
 dofile(maidroid_tool.modpath .. "/crafting.lua")
